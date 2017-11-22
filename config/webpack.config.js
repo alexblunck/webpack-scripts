3f8050c209372f986314bb36687399e44fe0ad5c
Alexander Blunck <alex@blunck.co>
2017-11-22 18:30:55 +0100

    Config: Added case-sensitive-paths-webpack-plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin')
 const merge = require('webpack-merge')
 
 module.exports = function (options) {
@@ -16,7 +17,8 @@ module.exports = function (options) {
         plugins: [
             new webpack.DefinePlugin({
               'process.env.NODE_ENV': JSON.stringify(environment)
-            })
+            }),
+            new CaseSensitivePathsPlugin()
         ],
         stats: true
     }
